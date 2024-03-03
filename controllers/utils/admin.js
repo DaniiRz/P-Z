6f90192c50a9171056f5b97a2757bf9af5c12e08
CarlosQuitanillaET <149823906+CarlosQuitanillaET@users.noreply.github.com>
2024-03-03 03:06:44 -0600

    Creacion de paginas faltantes(sin trabajar)

--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -35,7 +35,8 @@ const loadTemplate = async () => {
                             <a class="navbar-brand" href="dashboard.html">
                                 <img src="../../resources/img/logo.png" alt="CoffeeShop" width="50">
                             </a>
-                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                            data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
                             <div class="collapse navbar-collapse" id="navbarContent">
