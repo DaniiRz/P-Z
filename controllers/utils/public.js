c32dbdc21518f5ed236015ee62577fe15a2acd51
Jhonnyy0 <136754303+Jhonnyy0@users.noreply.github.com>
2024-03-12 11:59:19 -0600

    Navbar publico responsive

--- controllers/utils/public.js
+++ controllers/utils/public.js
@@ -2,48 +2,55 @@
 
 function generarMenuIndex() {
 
-    const menuIndex = `<div id="navbar fixed-top">
-    <nav class="navbar navbar-light bg-light">
+    const menuIndex = `
+    <div id="navbar fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="../admin/index.html">
                 <img src="../../resources/img/pull&zara (5).png" alt="" width="120" height="100">
             </a>
            
-            <ul class="navbar-nav d-flex flex-row justify-content-center me-5">
-                <li class="nav-item me-5 pe-5">
-                    <a class="nav-link active" aria-current="page" href="#"> Blusas </a>
-                </li>
-                <li class="nav-item me-5 pe-5">
-                    <a class="nav-link active" aria-current="page" href="#"> Jeans </a>
-                </li>
-                <li class="nav-item me-5 pe-5">
-                    <a class="nav-link active" aria-current="page" href="#"> Shorts </a>
-                </li>
-                <li class="nav-item me-5 pe-5">
-                    <a class="nav-link active" aria-current="page" href="#"> Abrigos </a>
-                </li>
-                <li class="nav-item me-5 pe-5">
-                    <a class="nav-link active" aria-current="page" href="#"> Vestidos </a>
-                </li>
-                <div class="iconHome ms-5 ps-5">
-                    <li class="nav-item me-3 me-lg-0">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page" href="#"> Blusas </a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page" href="#"> Jeans </a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page" href="#"> Shorts </a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page" href="#"> Abrigos </a>
+                    </li>
+                    <li class="nav-item ms-5 pe-5">
+                        <a class="nav-link active" aria-current="page" href="#"> Vestidos </a>
+                    </li>
+                </ul>
+                
+                <ul class="navbar-nav d-flex flex-row justify-content-auto">
+                    <li class="nav-item ms-5">
                         <a class="nav-link text-white" href="../admin/index.html">
-                            <img src="../../resources/img/casa 1.png" alt="" width="30" height="30">
+                            <img class="navbar-image" src="../../resources/img/casa 1.png" alt="" width="30" height="30">
                         </a>
                     </li>
-                </div>
- 
-                <div class="iconProfile">
-                    <li class="nav-item me-lg-0"> <!-- Cambiamos la clase de margen aquí -->
+                    <li class="nav-item ms-3">
                         <a class="nav-link text-white" href="../admin/perfil_admin.html">
-                            <img src="../../resources/img/usuario.png" alt="" width="30" height="30">
+                            <img class="navbar-image" src="../../resources/img/usuario.png" alt="" width="30" height="30">
                         </a>
                     </li>
-                </div>
-            </ul>
+                </ul>
+            </div>
         </div>
     </nav>
-</div>`;
+</div>
+
+  `;
     return menuIndex;
 
 }
