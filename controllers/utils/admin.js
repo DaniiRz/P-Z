b746ca124550a307d6e2e4810f0e46518a56c59b
L00MYY <160758553+L00MYY@users.noreply.github.com>
2024-03-16 02:33:51 -0600

    arreglo de pantalla inicio admin
    Se modifico el responsive de la pagina principal

--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -20,31 +20,65 @@ function generarMenuIndex() {
 
     const menuContent = `
     
-    <div id="navbar fixed-top">
-        <nav class="navbar navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="../admin/principal_admin.html">
-                    <img src="../../resources/img/pull&zara (5).png" alt="" width="130" height="110">
-                </a>
-                <ul class="navbar-nav d-flex flex-row me-1">
-                    <div class="iconHome">
-                        <li class="nav-item me-3 me-lg-0">
-                            <a class="nav-link text-white" href="../admin/principal_admin.html">
-                                <img src="../../resources/img/casa 1.png" alt="" width="30" height="30">
-                            </a>
-                        </li>
-                    </div>
-                    <div class="iconProfile">
-                        <li class="nav-item me-lg-0"> <!-- Cambiamos la clase de margen aquí -->
-                            <a class="nav-link text-white" href="../admin/perfil_admin.html">
-                                <img src="../../resources/img/usuario.png" alt="" width="30" height="30">
-                            </a>
-                        </li>
-                    </div>
+    <div id="navbar" class="fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-on-scroll">
+        <div class="container">
+            <a class="navbar-brand" href="../admin/index.html">
+                <img src="../../resources/img/pull&zara (5).png" alt="" width="120" height="100">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../admin/admin_usuarios.html">Usuarios</a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../admin/admin_categorias.html">Categorias</a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../admin/admin_talla.html">Tallas</a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../admin/admin_reseñas.html">Reseñas</a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../admin/admin_productos.html">Productos</a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../admin/admin_pedidos.html">Pedidos</a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../admin/perfil_admin.html">Administracion</a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link text-white" href="../admin/index.html">
+                            <img class="navbar-image" src="../../resources/img/casa 1.png" alt="" width="30"
+                                height="30">
+                        </a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link text-white" href="../admin/perfil_admin.html">
+                            <img class="navbar-image" src="../../resources/img/usuario.png" alt="" width="30"
+                                height="30">
+                        </a>
+                    </li>
                 </ul>
             </div>
-        </nav>
-    </div>`;
+        </div>
+    </nav>
+</div>`;
 
     return menuContent;
 
@@ -53,33 +87,48 @@ function generarMenuIndex() {
 function generarFooterIndex() {
 
     const footerIndex = `
-<div class="container">
-    <div class="row align-items-center">
+    <footer class="bg-dark text-white">
+    <div class="container">
+        <div class="row align-items-center">
             <div class="col-md-6">
                 <img src="../../resources/img/pull&zarablanco.png" alt="pull&zara logo" class="logo mb-1"
-                width="60px" height="60px">
+                     width="60px" height="60px">
                 <p class="text-white">Design amazing digital experiences that create more happy in the world</p>
             </div>
-        <div class="col-md-6 d-flex justify-content-end align-items-center">
-            <div class="social-icons align-self-start">
-                <a href="https://www.facebook.com" target="_blank"><img
-                    src="../../resources/img/facebook.png" alt="Facebook"></a>
-                <a href="https://www.instagram.com" target="_blank"><img
-                    src="../../resources/img/instagram.png" alt="Instagram"></a>
-                <a href="https://twitter.com" target="_blank"><img src="../../resources/img/x.png"
-                    alt="X"></a>
-                <a href="https://www.tiktok.com/" target="_blank"><img src="../../resources/img/tik-tok.png"
-                    alt="Tik Tok"></a>
+            <div class="col-md-6 d-flex justify-content-end align-items-center">
+                <div class="social-icons align-self-start">
+                    <a href="https://www.facebook.com" target="_blank"><img
+                                src="../../resources/img/facebook.png" alt="Facebook"></a>
+                    <a href="https://www.instagram.com" target="_blank"><img
+                                src="../../resources/img/instagram.png" alt="Instagram"></a>
+                    <a href="https://twitter.com" target="_blank"><img src="../../resources/img/x.png"
+                                                                         alt="X"></a>
+                    <a href="https://www.tiktok.com/" target="_blank"><img src="../../resources/img/tik-tok.png"
+                                                                             alt="Tik Tok"></a>
+                </div>
             </div>
         </div>
-</div>
-
-    <div class="row">
-        <div class="col">
-            <p class="text-muted small">© 2024 PULL & ZARA. Todos los derechos reservados.</p>
+        <div class="row mt-4">
+            <div class="col">
+                <p class="text-muted small">© 2024 PULL & ZARA. Todos los derechos reservados.</p>
+            </div>
         </div>
-    </div>`;
+    </div>
+</footer>
+`;
     return footerIndex;
 
 }
+
+//* diseño nav//
+
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
 
