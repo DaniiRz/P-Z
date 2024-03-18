d275ca3fb1a62104d2c661ce10dd47439391feef
Daniela <20220434@ricaldone.edu.sv>
2024-03-18 08:10:18 -0600

    footer arreglado en catalogos, icono de carrito agregado al navbar y linkeado con la pagina de carrito

--- controllers/utils/public.js
+++ controllers/utils/public.js
@@ -4,51 +4,66 @@ function generarMenuPublic() {
 
     const menuIndexPublic = `
     <div id="navbar fixed-top">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container">
-                <a class="navbar-brand" href="../public/index.html">
-                    <img src="../../resources/img/pull&zara (5).png" alt="" width="120" height="100">
-                </a>
-           
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            
-                <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item ms-5">
-                <a class="nav-link active" aria-current="page" href="../../views/public/catalogo_blusas.html"> Blusas </a>
-            </li>
-            <li class="nav-item ms-5">
-                <a class="nav-link active" aria-current="page" href="../../views/public/catalogo_jeans.html"> Jeans </a>
-            </li>
-            <li class="nav-item ms-5">
-                <a class="nav-link active" aria-current="page" href="../../views/public/catalogo_shorts.html"> Shorts </a>
-            </li>
-            <li class="nav-item ms-5">
-                <a class="nav-link active" aria-current="page" href="../public/catalogo_abrigos.html"> Abrigos </a>
-            </li>
-            <li class="nav-item ms-5 pe-5">
-                <a class="nav-link active" aria-current="page" href="../public/catalogo_vestidos.html"> Vestidos </a>
-            </li>
-        </ul>
-                
-                    <ul class="navbar-nav d-flex flex-row justify-content-auto">
-                        <li class="nav-item ms-5">
-                            <a class="nav-link text-white" href="../public/index.html">
-                                <img class="navbar-image" src="../../resources/img/casa 1.png" alt="" width="30" height="30">
-                            </a>
-                        </li>
-                        <li class="navbar-nav d-flex flex-row justify-content-auto">
-                            <a class="nav-link text-white" href="../public/perfil_usuario.html">
-                                <img class="navbar-image" src="../../resources/img/usuario.png" alt="" width="30" height="30">
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="../public/index.html">
+                <img src="../../resources/img/pull&zara (5).png" alt="" width="120" height="100">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../../views/public/catalogo_blusas.html"> Blusas </a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../../views/public/catalogo_jeans.html"> Jeans </a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page"
+                            href="../../views/public/catalogo_shorts.html"> Shorts </a>
+                    </li>
+                    <li class="nav-item ms-5">
+                        <a class="nav-link active" aria-current="page" href="../public/catalogo_abrigos.html">
+                            Abrigos </a>
+                    </li>
+                    <li class="nav-item ms-5 pe-5">
+                        <a class="nav-link active" aria-current="page" href="../public/catalogo_vestidos.html">
+                            Vestidos </a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav d-flex flex-row justify-content-auto">
+                    <li class="nav-item ms-5">
+                        <a class="nav-link text-white" href="../public/index.html">
+                            <img class="navbar-image" src="../../resources/img/casa 1.png" alt="" width="30"
+                                height="30">
+                        </a>
+                    </li>
+                    <li class="navbar-nav d-flex flex-row justify-content-auto">
+                        <a class="nav-link text-white" href="../public/perfil_usuario.html">
+                            <img class="navbar-image" src="../../resources/img/usuario.png" alt="" width="30"
+                                height="30">
+                        </a>
+                    </li>
+                    <li class="navbar-nav d-flex flex-row justify-content-auto">
+                        <a class="nav-link text-white" href="../public/carrito_compras.html">
+                            <img class="navbar-image" src="../../resources/img/carrito-de-compras.png" alt="" width="30"
+                                height="30">
+                        </a>
+                    </li>
+                </ul>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
+</div>
+
     `;
     return menuIndexPublic;
 }
