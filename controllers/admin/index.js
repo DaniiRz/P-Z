3f9ae2a42195758de94b2fe21653e6022508215a
Daniela <20220434@ricaldone.edu.sv>
2024-05-05 18:38:14 -0600

    validacion de inicio son usuario predefinido dese javascript y arreglos en ortografia

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,7 +1,53 @@
-<script src="../../controllers/utils/admin.js"></script>
-// Método del evento para cuando el documento ha cargado.
-document.addEventListener('DOMContentLoaded', async () => {
-    // Llamada a la función para mostrar el encabezado y pie del documento.
-    loadTemplate();
-});
+// Valores definidos para realizar el inicio de sesión
+var correoPredefinido = "admin@ricaldone.edu.sv";
+var contrasenaPredefinida = "Rical_2024";
 
+function validarInicios() {
+    var correo = document.getElementById('correo').value;
+    var contrasena = document.getElementById('contrasena').value;
+
+    // Verificar que los campos no estén vacíos y si lo están, enviar una advertencia
+    if (correo.trim() === '') {
+        Swal.fire({
+            icon: 'warning',
+            text: 'Por favor, ingrese su correo electrónico.'
+        });
+        return;
+    }
+    if (contrasena.trim() === '') {
+        Swal.fire({
+            icon: 'warning',
+            text: 'Por favor, ingrese su contraseña.'
+        });
+        return;
+    }
+
+    // Verificar que la sintaxis del correo sea correcta
+    var correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!correoRegex.test(correo)) {
+        Swal.fire({
+            icon: 'error',
+            text: 'Por favor, ingrese un correo electrónico válido.'
+        });
+        return;
+    }
+
+    // Verificar si el correo electrónico y la contraseña coinciden con los predefinidos
+    if (correo !== correoPredefinido || contrasena !== contrasenaPredefinida) {
+        Swal.fire({
+            icon: 'error',
+            text: 'Correo electrónico o contraseña incorrectos.'
+        });
+        return;
+    }
+
+    Swal.fire({
+        icon: 'success',
+        text: 'Inicio de sesión exitoso. ¡Bienvenido!'
+    });
+
+    // Redireccionar a otra página después de 2 segundos
+    setTimeout(function() {
+        window.location.href = "../admin/principal_admin.html";
+    }, 1000);
+}
