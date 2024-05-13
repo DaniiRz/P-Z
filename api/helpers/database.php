ad45104d7ed1ff6c318fc5b9f1b93085bffaeffe
Jhonnyy0 <136754303+Jhonnyy0@users.noreply.github.com>
2024-05-13 11:16:28 -0600

    Crud de productos y archivo de validaciones

--- api/helpers/database.php
+++ api/helpers/database.php
@@ -26,13 +26,25 @@ class Database
             self::$statement = self::$connection->prepare($query);
             // Se ejecuta la sentencia preparada y se retorna el resultado.
             return self::$statement->execute($values);
-        } catch (PDOException $error) {
+        } 
+        
+        catch (PDOException $error) {
             // Se obtiene el código y el mensaje de la excepción para establecer un error personalizado.
             self::setException($error->getCode(), $error->getMessage());
             return false;
         }
     }
 
+    public static function getRow($query, $values = null)
+    {
+        if (self::executeRow($query, $values)) {
+            return self::$statement->fetch(PDO::FETCH_ASSOC);
+        } 
+        
+        else {
+            return false;
+        }
+    }
 
     /*
      *   Método para establecer un mensaje de error personalizado al ocurrir una excepción.
