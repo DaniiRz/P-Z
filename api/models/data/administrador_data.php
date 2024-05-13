8d9b5e877270759671ba9657b4b63456779d9be8
Axel <20220127@ricaldone.edu.sv>
2024-05-13 13:58:23 -0600

    se elimino funcion de alias en data admin

--- api/models/data/administrador_data.php
+++ api/models/data/administrador_data.php
@@ -67,20 +67,6 @@ class AdministradorData extends AdministradorHandler
         }
     }
 
-    public function setAlias($value, $min = 6, $max = 25)
-    {
-        if (!Validator::validateAlphanumeric($value)) {
-            $this->data_error = 'El alias debe ser un valor alfanumérico';
-            return false;
-        } elseif (Validator::validateLength($value, $min, $max)) {
-            $this->alias = $value;
-            return true;
-        } else {
-            $this->data_error = 'El alias debe tener una longitud entre ' . $min . ' y ' . $max;
-            return false;
-        }
-    }
-
     public function setClave($value)
     {
         if (Validator::validatePassword($value)) {
