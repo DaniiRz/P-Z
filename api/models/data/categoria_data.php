385935fb8b6506e50c8878c8d74eaa22b56136c9
Axel <20220434@ricaldone.edu.sv>
2024-05-22 10:01:02 -0600

    categorias crud (Dani)

--- api/models/data/categoria_data.php
+++ api/models/data/categoria_data.php
@@ -1,43 +1,42 @@
 <?php
-
-// Se incluyen las clases
+// Se incluye la clase para validar los datos de entrada.
 require_once('../../helpers/validator.php');
-require_once('../../models/handler/productos_handler.php');
+// Se incluye la clase padre.
+require_once('../../models/handler/categoria_handler.php');
+/*
+ *  Clase para manejar el encapsulamiento de los datos de la tabla CATEGORIA.
+ */
 
-// Clase para manejar el encapsulamiento de datos
-class SubCategoriaData extends SubCategoriaHandler
-{
-    
-    // Artributo adicional
-    private $data_error = null;
+ class CategoriaData extends CategoriaHandler{
 
-    // Metodos para validar y establecer los datos
-    public function setIdCategoria($value){
-        if (Validator::validateNaturalNumber($value)) {
-            $this->id = $value;
-            return true;
-        } 
-        
-        else {
-            $this->data_error = 'El identificador de la categoria es incorrecto';
-            return false;
+    private $data_error = null; 
+    private $filename = null; 
+
+    //metodos para validar los datos
+    public function setId($value){
+        if(Validator:: validateNaturalNumber($value)){
+            $this -> id = $value;
+            return true; 
+
+        }else{
+            $this -> data_error = 'Identificador de categoría incorrecto'; 
+            return false; 
         }
     }
 
-    public function setNombreCategoria($value, $min = 2, $max= 255){
-        if (!Validator::validateAlphanumeric($value)) {
+    public function setNombre($value, $min = 4, $max =30){
+        
+        if(!Validator::validateAlphanumeric($value)){
             $this->data_error = 'El nombre debe ser un valor alfanumérico';
             return false;
-        } 
-        
-        elseif (Validator::validateLength($value, $min, $max)) {
-            $this->nombre = $value;
+        }elseif (Validator::validateLength($value, $min, $max)) {
+            $this -> nombre = $value;
             return true;
-        } 
-        
-        else {
-            $this->data_error = 'El nombre debe tener una longitud entre ' . $min . ' y ' . $max;
-            return false;
+          }else{
+            $this-> data_error = 'El nombre debe de tener una longitud entre' .$min. 'y' .$max; 
+            return false; 
         }
     }
-}
+
+
+ }
