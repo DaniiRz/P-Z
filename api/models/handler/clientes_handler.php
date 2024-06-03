d7a3463bc63336f5ff339cbe1bd74fe3152ae531
rebiiu <160758553+L00MYY@users.noreply.github.com>
2024-06-03 13:56:50 -0600

    Se creo data y handler de clientes

---
+++ api/models/handler/clientes_handler.php
@@ -0,0 +1,139 @@
+<?php
+// Se incluye la clase para trabajar con la base de datos.
+require_once('../../helpers/database.php');
+/*
+ *  Clase para manejar el comportamiento de los datos de la tabla clientes.
+ */
+
+class ClienteHandler{
+    /*
+     *  Declaración de atributos para el manejo de datos.
+     */
+    protected $id = null;
+    protected $nombre = null;
+    protected $apellido = null;
+    protected $dui = null;
+    protected $telefono = null;
+    protected $correo = null;
+    protected $clave = null;
+
+
+    /*
+     *  Métodos para gestionar la cuenta del cliente.
+     */
+
+    public function checkPassword($clave)
+    {
+        $sql = 'SELECT clave_cliente
+                FROM tb_clientes
+                WHERE id_cliente = ?';
+        $params = array($_SESSION['idCliente']);
+        $data = Database::getRow($sql, $params);
+        // Se verifica si la contraseña coincide con el hash almacenado en la base de datos.
+        if (password_verify($clave, $data['clave_cliente'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function changePassword()
+    {
+        $sql = 'UPDATE tb_clientes
+                SET clave_cliente = ?
+                WHERE id_cliente = ?';
+        $params = array($this->clave, $_SESSION['idCliente']);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function checkUser($correo, $clave)
+    {
+        $sql = 'SELECT id_cliente, correo_cliente, clave_cliente
+                FROM tb_clientes
+                WHERE correo_cliente = ?';
+        $params = array($correo);
+        if (!($data = Database::getRow($sql, $params))) {
+            return false;
+        } elseif (password_verify($clave, $data['clave_cliente'])) {
+            $_SESSION['idCliente'] = $data['id_cliente'];
+            $_SESSION['correoCliente'] = $data['correo_cliente'];
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /*
+     *  Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
+     */
+    public function searchRows() {
+        $value = '%' . Validator::getSearchValue() . '%';
+        $sql = 'SELECT id_cliente, nombre_cliente, apellido_cliente, correo_cliente
+                FROM tb_clientes
+                WHERE apellido_cliente LIKE ? OR nombre_cliente LIKE ?
+                ORDER BY apellido_cliente';
+        $params = array($value, $value);
+        return Database::getRows($sql, $params);
+    }
+
+    public function readProfile()
+    {
+        $sql = 'SELECT id_cliente, nombre_cliente, apellido_cliente, correo_cliente
+                FROM tb_clientes
+                WHERE id_cliente = ?';
+        $params = array($_SESSION['idCliente']);
+        return Database::getRow($sql, $params);
+    }
+
+    public function editProfile()
+    {
+        $sql = 'UPDATE tb_clientes
+                SET nombre_cliente = ?, apellido_cliente = ?, correo_cliente = ?
+                WHERE id_cliente = ?';
+        $params = array($this->nombre, $this->apellido, $this->correo, $_SESSION['nombreCliente']);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function createRow()
+    {
+        $sql = 'INSERT INTO tb_clientes(nombre_cliente, apellido_cliente, dui_cliente, telf_cliente, correo_cliente, clave_cliente)
+                VALUES(?, ?, ?, ?, ?, ?)';
+        $params = array($this->nombre, $this->apellido, $this->dui, $this->telefono, $this->correo, $this->clave);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function readAll()
+    {
+        $sql = 'SELECT id_cliente, nombre_cliente, apellido_cliente, correo_cliente
+                FROM tb_clientes
+                ORDER BY apellido_cliente';
+        return Database::getRows($sql);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT id_cliente, nombre_cliente, apellido_cliente, correo_cliente FROM tb_clientes
+                WHERE id_cliente = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    public function updateRow()
+    {
+        $sql = 'UPDATE tb_clientes
+                SET nombre_cliente = ?, apellido_cliente = ?, correo_cliente = ?
+                WHERE id_cliente = ?';
+        $params = array($this->nombre, $this->apellido, $this->correo, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM tb_clientes
+                WHERE id_cliente = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+}
