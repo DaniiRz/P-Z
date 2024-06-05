9c571a946faa5088899cc12d05a57f8dda45945b
Daniela <20220434@ricaldone.edu.sv>
2024-06-05 16:47:44 -0600

    cambios en header

--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -32,9 +32,6 @@ function generarMenuIndex() {
                                     <a class="nav-link active" aria-current="page" href="../admin/admin_categoria.html">Categorías</a>
                                 </li>
                                 <li class="nav-item ms-5">
-                                    <a class="nav-link active" aria-current="page" href="../admin/admin_subcategorias.html">SubCategorías</a>
-                                </li>
-                                <li class="nav-item ms-5">
                                     <a class="nav-link active" aria-current="page" href="../admin/admin_reseñas.html">Reseñas</a>
                                 </li>
                                 <li class="nav-item ms-5">
