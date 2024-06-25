f81eba45c208301c2af23d709366b5b971a1d947
L00MYY <160758553+L00MYY@users.noreply.github.com>
2024-06-25 12:01:25 -0600

    se elimino js ineccesario

--- controllers/utils/cargar_detalle.js
+++
@@ -1,9 +0,0 @@
-// Este evento se dispara cuando el contenido HTML ha sido completamente cargado
-document.addEventListener('DOMContentLoaded', function () {
-    
-    // Obtiene el elemento con el ID "detallesProducto"
-    const detalle = document.getElementById("detallesProducto");
-    
-    // Llama a la función detalleProducto() y coloca su resultado dentro del elemento detalle
-    detalle.innerHTML = detalleProducto();
-});
