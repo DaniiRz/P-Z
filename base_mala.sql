c8485ae776b42cbb2b2baf0a734905b921aa031d
DaniiRz <106101240+DaniiRz@users.noreply.github.com>
2024-07-16 15:14:30 -0600

    se separaron las bases en archivos diferentes

--- base_mala.sql
+++ base_mala.sql
@@ -1,12 +1,12 @@
--- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: pull_and_zara
+
+
+/*VERSION ANTERIOR POR SI LA VERSION DE BASE DE DATOS CON INSERCIONES NO FUNCIONA*/
+
 -- ------------------------------------------------------
 -- Server version	10.4.32-MariaDB
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_C0ONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
@@ -14,15 +14,12 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `tb_admins`
 --
 DROP DATABASE if EXISTS pull_and_zara;
 CREATE DATABASE pull_and_zara; 
-
 USE pull_and_zara; 
-
 DROP TABLE IF EXISTS `tb_admins`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -36,23 +33,18 @@ CREATE TABLE `tb_admins` (
   UNIQUE KEY `rest_correo_unico` (`correo_admin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_admins`
 --
-
 -- INSERT INTO pedido(direccion_pedido, id_cliente) VALUES((SELECT direccion_cliente FROM cliente WHERE id_cliente = ?), ?);
 -- INSERT INTO tb_sub_categorias(nombre_subcategoria, imagen_subcategoria, id_categoria) VALUES((SELECT direccion_cliente FROM cliente WHERE id_cliente = ?), ?);
-
 LOCK TABLES `tb_admins` WRITE;
 /*!40000 ALTER TABLE `tb_admins` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_admins` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_categorias`
 --
-
 DROP TABLE IF EXISTS `tb_categorias`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -64,19 +56,17 @@ CREATE TABLE `tb_categorias` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+SELECT * FROM tb_categorias;
 --
 -- Dumping data for table `tb_categorias`
 --
-
 LOCK TABLES `tb_categorias` WRITE;
 /*!40000 ALTER TABLE `tb_categorias` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_categorias` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_clientes`
 --
-
 DROP TABLE IF EXISTS `tb_clientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -88,7 +78,7 @@ CREATE TABLE `tb_clientes` (
   `telf_cliente` varchar(12) NOT NULL,
   `correo_cliente` varchar(255) NOT NULL,
   `clave_cliente` varchar(255) NOT NULL,
-  `genero_cliente` ENUM('Femenino', 'Masculino') NOT NULL,
+  `direccion_cliente` VARCHAR(250) NOT NULL, 
   `estado_cliente` tinyint(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id_cliente`),
   UNIQUE KEY `dui_client` (`dui_client`),
@@ -99,20 +89,16 @@ CREATE TABLE `tb_clientes` (
   UNIQUE KEY `rest_correo_cliente_unico` (`correo_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_clientes`
 --
-
 LOCK TABLES `tb_clientes` WRITE;
 /*!40000 ALTER TABLE `tb_clientes` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_clientes` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_colores`
 --
-
 DROP TABLE IF EXISTS `tb_colores`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -122,20 +108,16 @@ CREATE TABLE `tb_colores` (
   PRIMARY KEY (`id_color`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_colores`
 --
-
 LOCK TABLES `tb_colores` WRITE;
 /*!40000 ALTER TABLE `tb_colores` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_colores` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_detalle_pedido`
 --
-
 DROP TABLE IF EXISTS `tb_detalle_pedido`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -153,20 +135,16 @@ CREATE TABLE `tb_detalle_pedido` (
   CONSTRAINT `rest_check_cantidad_producto` CHECK (`cantidad_producto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_detalle_pedido`
 --
-
 LOCK TABLES `tb_detalle_pedido` WRITE;
 /*!40000 ALTER TABLE `tb_detalle_pedido` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_detalle_pedido` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_detalle_productos`
 --
-
 DROP TABLE IF EXISTS `tb_detalle_productos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -183,25 +161,21 @@ CREATE TABLE `tb_detalle_productos` (
   KEY `fk_id_talla_id_detalle` (`id_talla`),
   KEY `fk_id_producto_id_detalle` (`id_producto`),
   CONSTRAINT `fk_id_color_id_detalle` FOREIGN KEY (`id_color`) REFERENCES `tb_colores` (`id_color`),
-  CONSTRAINT `fk_id_producto_id_detalle` FOREIGN KEY (`id_producto`) REFERENCES `tb_productos` (`id_producto`) ON DELETE CASCADE, 
+  CONSTRAINT `fk_id_producto_id_detalle` FOREIGN KEY (`id_producto`) REFERENCES `tb_productos` (`id_producto`) ON DELETE CASCADE,
   CONSTRAINT `fk_id_talla_id_detalle` FOREIGN KEY (`id_talla`) REFERENCES `tb_tallas` (`id_talla`),
   CONSTRAINT `rest_check_existencias` CHECK (`existencias`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_detalle_productos`
 --
-
 LOCK TABLES `tb_detalle_productos` WRITE;
 /*!40000 ALTER TABLE `tb_detalle_productos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_detalle_productos` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_estado_valo`
 --
-
 DROP TABLE IF EXISTS `tb_estado_valo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -211,20 +185,16 @@ CREATE TABLE `tb_estado_valo` (
   PRIMARY KEY (`id_estado_valo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_estado_valo`
 --
-
 LOCK TABLES `tb_estado_valo` WRITE;
 /*!40000 ALTER TABLE `tb_estado_valo` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_estado_valo` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_pedidos`
 --
-
 DROP TABLE IF EXISTS `tb_pedidos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -239,21 +209,16 @@ CREATE TABLE `tb_pedidos` (
   CONSTRAINT `fk_id_cliente_id_pedido` FOREIGN KEY (`id_cliente`) REFERENCES `tb_clientes` (`id_cliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_pedidos`
 --
-
-
 LOCK TABLES `tb_pedidos` WRITE;
 /*!40000 ALTER TABLE `tb_pedidos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_pedidos` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_productos`
 --
-
 DROP TABLE IF EXISTS `tb_productos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -268,28 +233,20 @@ CREATE TABLE `tb_productos` (
   CONSTRAINT `fk_id_producto_id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `tb_categorias` (`id_categoria`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_productos`
 --
-
 LOCK TABLES `tb_productos` WRITE;
 /*!40000 ALTER TABLE `tb_productos` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_productos` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_sub_categorias`
 --
-
-
 /*!40101 SET character_set_client = @saved_cs_client */;
-
-
 --
 -- Table structure for table `tb_tallas`
 --
-
 DROP TABLE IF EXISTS `tb_tallas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -299,20 +256,16 @@ CREATE TABLE `tb_tallas` (
   PRIMARY KEY (`id_talla`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_tallas`
 --
-
 LOCK TABLES `tb_tallas` WRITE;
 /*!40000 ALTER TABLE `tb_tallas` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_tallas` ENABLE KEYS */;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `tb_valoracion`
 --
-
 DROP TABLE IF EXISTS `tb_valoracion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -329,17 +282,14 @@ CREATE TABLE `tb_valoracion` (
   CONSTRAINT `fk_id_estado_valo_id_valoracion` FOREIGN KEY (`id_estado_valo`) REFERENCES `tb_estado_valo` (`id_estado_valo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `tb_valoracion`
 --
-
 LOCK TABLES `tb_valoracion` WRITE;
 /*!40000 ALTER TABLE `tb_valoracion` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tb_valoracion` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
@@ -349,64 +299,6 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2024-05-15 17:04:33
-
--- Inserción en tb_categorias
-INSERT INTO tb_categorias (nombre_categoria, imagen_categoria)
-VALUES ('Camisa', '667923961abbb.png'),
-       ('Calzado', '667923961abbb.png'),
-       ('Accesorios', '667923961abbb.png');
-
--- Inserción en tb_colores
-INSERT INTO tb_colores (nombre_color)
-VALUES ('Rojo'),
-       ('Azul'),
-       ('Verde'),
-       ('Amarillo');
--- Inserción en tb_tallas
-INSERT INTO tb_tallas (numero_talla)
-VALUES ('S'),
-       ('M'),
-       ('L');
-       -- Inserción en tb_productos
-INSERT INTO tb_productos (nombre_producto, desc_producto, fecha_registro_produc, id_categoria)
-VALUES ('Camisa Casual Azul', 'Camisa de algodón para hombre', NOW(), 1),
-       ('Zapatos Deportivos Negros', 'Zapatos para correr con tecnología avanzada', NOW(), 2),
-       ('Bolso de Cuero Marrón', 'Bolso elegante para mujer', NOW(), 3);
-
-       
--- Inserción en tb_detalle_productos
-INSERT INTO tb_detalle_productos (existencias, img_producto, id_color, id_talla, id_producto, precio_producto)
-VALUES (50, '66560f818bd86.png', 1, 1, 1, 49.99),
-       (30, '66560f818bd86.png', 2, 2, 2, 59.99),
-       (20, '66560f818bd86.png', 3, 3, 3, 39.99);
-
--- Inserción en tb_pedidos
-INSERT INTO tb_pedidos (fecha_pedido, estado_pedido, direccion_pedido, id_cliente)
-VALUES ('2024-06-01 10:00:00', 'Pendiente', 'Calle Principal 123', NULL),
-       ('2024-06-02 11:00:00', 'Finalizado', 'Avenida Central 456', NULL),
-       ('2024-06-03 12:00:00', 'Entregado', 'Boulevard Norte 789', NULL);
-
-
-
-
--- Inserción en tb_estado_valo
-INSERT INTO tb_estado_valo (estado_valo)
-VALUES ('Activo'),
-       ('Inactivo');
-
--- Inserción en tb_detalle_pedido
-INSERT INTO tb_detalle_pedido (cantidad_producto, precio_producto, id_pedido, id_detalle_producto)
-VALUES (2, 49.99, 1, 1),
-       (1, 59.99, 2, 2),
-       (3, 39.99, 3, 3);
-
--- Inserción en tb_valoracion
-INSERT INTO tb_valoracion (comentario_cliente, fecha_valoracion, id_detalle_p, id_estado_valo)
-VALUES ('Buen producto, excelente calidad.', NOW(), 1, 1),
-       ('Envío rápido, muy satisfecho con el servicio.', NOW(), 2, 1),
-       ('Producto llegó dañado, espero una solución rápida.', NOW(), 3, 2);
-
-
 
 
 SELECT * FROM tb_admins;
@@ -420,13 +312,5 @@ SELECT * FROM tb_pedidos;
 SELECT * FROM tb_productos;
 SELECT * FROM tb_tallas;
 SELECT * FROM tb_valoracion;
-
-
-
-
-
-
-
-
-
-
+            
+                
