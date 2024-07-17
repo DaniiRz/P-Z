76f7a8d601facf071ec03749fdaf86688cab515a
DaniiRz <106101240+DaniiRz@users.noreply.github.com>
2024-07-17 09:42:48 -0600

    reporte administradores terminado

--- api/reports/admin/administrador.php
+++ api/reports/admin/administrador.php
@@ -18,7 +18,8 @@ if ($dataAdministradores = $administrador->readAll()) {
     $pdf->setFont('Arial', 'B', 11); 
     // Se imprimen las celdas con los encabezados.
     $pdf->cell(60, 10, 'Nombre', 1, 0, 'C', 1);
-    $pdf->cell(60, 10, 'Correo Electrónico', 1, 0, 'C', 1);
+    $pdf->cell(60, 10, 'Apellido', 1, 0, 'C', 1);
+    $pdf->cell(60, 10, $pdf->encodeString('Correo electrónico'), 1, 1, 'C', 1);
 
     // Se establece la fuente para los datos de los administradores.
     $pdf->setFont('Arial', '', 11);
@@ -26,12 +27,12 @@ if ($dataAdministradores = $administrador->readAll()) {
     // Se recorren los registros fila por fila.
     foreach ($dataAdministradores as $rowAdministrador) {
         // Se imprimen las celdas con los datos de los administradores.
-        $pdf->cell(60, 10, $pdf->encodeString($rowAdministrador['nombre']), 1, 0);
-        $pdf->cell(60, 10, $pdf->encodeString($rowAdministrador['correo']), 1, 0);
+        $pdf->cell(60,10, $rowAdministrador['nombre_admin'], 1, 0); 
+        $pdf->cell(60,10, $rowAdministrador['apellido_admin'], 1, 0); 
+        $pdf->cell(60,10, $rowAdministrador['correo_admin'], 1, 1); 
     }
 } else {
     $pdf->cell(0, 10, $pdf->encodeString('No hay administradores para mostrar'), 1, 1);
 }
 // Se llama implícitamente al método footer() y se envía el documento al navegador web.
 $pdf->output('I', 'administradores.pdf');
-?>
