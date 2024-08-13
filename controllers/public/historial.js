c38525283516e7d4c5e6ff8dc66e91283f1d6a8f
DaniiRz <106101240+DaniiRz@users.noreply.github.com>
2024-08-13 11:28:40 -0600

    diseño cards historial compras

--- controllers/public/historial.js
+++ controllers/public/historial.js
@@ -7,7 +7,7 @@ const CARD = document.getElementById('cardBody')
 document.addEventListener('DOMContentLoaded', async () => {
     try {
         const DATA = await fetchData(HISTORIAL_API, 'readHistorial');
-        
+
         // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
         if (DATA.status) {
             // Se inicializa la card de pedidos.
@@ -15,16 +15,23 @@ document.addEventListener('DOMContentLoaded', async () => {
             // Se recorre el conjunto de registros fila por fila a través del objeto row.
             DATA.dataset.forEach(row => {
                 CARD.innerHTML += `
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">Pedido #${row.id_pedido}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">Fecha: ${row.fecha_pedido}</h6>
-                            <p class="card-text">Producto: ${row.nombre_producto}</p>
-                            <p class="card-text">Cantidad: ${row.cantidad_producto}</p>
-                            <p class="card-text">Precio Unitario: $${row.precio_producto}</p>
-                            <p class="card-text">Total: $${(row.cantidad_producto * row.precio_producto).toFixed(2)}</p>
+                   <div class="card card-custom mb-5">
+                                <div class="card-header card-header-custom">
+                                   <h6 class="card-title mb-2 text-center" >¡Gracias por su compra!</h6>
+                                 </div>
+                        <div class="card-body d-flex">
+                                <div class="flex-grow-1">
+                                    <h6 class="card-subtitle mb-2 text-muted-custom">Fecha: ${row.fecha_pedido}</h6>
+                                    <p class="card-text">Producto: ${row.nombre_producto}</p>
+                                    <p class="card-text">Cantidad: ${row.cantidad_producto}</p>
+                                    <p class="card-text">Precio Unitario: $${row.precio_producto}</p>
+                                    <p class="card-text">Total: $${(row.cantidad_producto * row.precio_producto).toFixed(2)}</p>
+                                </div>
+                                <div>
+                                    <img src="../../resources/img/cart (2).png" class="ml-5 mr-3" style="max-height: 150px; object-fit: cover;">
+                                </div>
                         </div>
-                    </div>
+                     </div>
                 `;
             });
         } else {
